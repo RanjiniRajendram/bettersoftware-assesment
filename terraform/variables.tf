6359c08e62c457952d3996c7b3945ec5d6ad52b6
Ranjani.R <21043675+RanjiniRajendram@users.noreply.github.com>
2025-10-05 18:13:46 +0530

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "southindia" 
+}
+
+variable "resource_group" {
+  type    = string
+  default = "rg-openwebui"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "aks-openwebui"
+}
